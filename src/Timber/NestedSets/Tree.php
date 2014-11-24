b222965b15d0487dd49333afe323cc0d4cccdf2b
Andrew Mason <andrew@nocturnal.net.au>
2014-11-24 22:58:10 +1030

    working query for getting path

--- src/Timber/NestedSets/Tree.php
+++ src/Timber/NestedSets/Tree.php
@@ -4,25 +4,26 @@ namespace Timber\NestedSets;
 use \Phalcon\Mvc\Model;
 use Phalcon\Mvc\Model\Resultset\Simple as ResultSet;
 use Modules\Products\Models\Categories as CategoriesModel;
-
+use \Timber\Model\Resultset\Object as ObjectResultSet;
 
 final class Tree extends Model 
-{
+{ 
     protected $conn = null;
     public $log     = null;
     
     public $id;
     public $foreign_id;
-    public $tableName = 'category_hierarchy';
+    public $tableName   = 'category_hierarchy';
+    protected $dialect  = null;
     
     
     public function initialize()
     {
   //      $this->hasOne('foreign_id', '\Modules\Products\Models\Categories', 'id');   
          $this->belongsTo('foreign_id', '\Modules\Products\Models\Categories', 'id', [
-            "foreignKey" => array(
+            "foreignKey" => [
                 "message" => "The part cannot be deleted because other robots are using it"
-            )
+            ]
         ]);
         $this->conn = $this->getReadConnection();
         
@@ -65,34 +66,39 @@ final class Tree extends Model
      */
     public function getPath($id)
     {
+        $params = ['id' => $id];
         $res = $this->_modelsManager->createBuilder()
             ->columns('child.foreign_id, child.id, cat.category_name')
             ->addFrom('Timber\NestedSets\Tree', 'child')
             ->addFrom('Timber\NestedSets\Tree', 'parent')
             ->join('\Modules\Products\Models\Categories', 'parent.foreign_id = cat.id', 'cat')
-            ->where('child.id = :id:', ['id'=>$id])
+            ->where('child.id = :id:', $params)
             ->andWhere('child.lft BETWEEN parent.lft AND parent.rgt')
             ->orderBy('parent.lft')
-            ->getQuery()
-            ->execute();
+            ->getQuery();
             
-            
-        return $res;
-  /*
-        $sql = 
-        'SELECT parent.foreign_id, cat.category_name
-        FROM category_hierarchy AS child,
-        category_hierarchy AS parent
-        INNER JOIN categories as cat ON parent.foreign_id = cat.id
-        WHERE child.lft
-        BETWEEN parent.lft
-        AND parent.rgt
-        AND child.id = :id
-        ORDER BY parent.lft';
-        return $this->conn->fetchAll($sql,\PDO::FETCH_ASSOC, ['id'=>$id]);
-        */
-    }// end get_path
+        $sql = $this->getRawSQLFromBuilder($res);
+        
+        $x = new ObjectResultSet(null, $this, $this->conn->query($sql, $params), null, null,'Module\Products\Path');
+        $x->setHydrateMode(Resultset::HYDRATE_OBJECTS);
+        return $x;
+        
+    }
 
+    
+    protected function getRawSQLFromBuilder(\Phalcon\Mvc\Model\Query $query)
+    {
+        $dialect = $this->conn->getDialect();
+        return $dialect->select($query->parse());
+    }
+    
+    protected function getDialect()
+    {
+        
+        
+    }
+    
+    
     
     /**
      * Returns a path to a given element
@@ -103,15 +109,15 @@ final class Tree extends Model
     {
         
         $query = "SELECT parent.ch_id
-                    FROM category_hierarchy AS child,
-                    category_hierarchy AS parent
-                    WHERE child.lft
-                    BETWEEN parent.lft
-                    AND parent.rgt
-                    AND child.ch_id = ".(int)$ch_id."
-                    AND parent.lft != 1
-                    ORDER BY parent.lft";
-        
+            FROM category_hierarchy AS child,
+            category_hierarchy AS parent
+            WHERE child.lft
+            BETWEEN parent.lft
+            AND parent.rgt
+            AND child.ch_id = ".(int)$ch_id."
+            AND parent.lft != 1
+            ORDER BY parent.lft";
+    
         
     }// end get_path
 
